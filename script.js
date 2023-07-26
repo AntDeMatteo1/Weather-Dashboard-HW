cd60094e1a36a8f765c93f4b44c49c1cee4efdd5
test <adematteo1998@gmail.com>
2023-07-25 20:57:44 -0400

    added variables to js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Global variables created
+var searchFormEl = document.querySelector("#search-form");
+var cityNameEl = document.querySelector("#city-name-input");
+var fiveDayForecastEl = document.querySelector("#five-day-forecast-container");
+var currentWeatherEl = document.querySelector("#current-weather-container");
+var forecastEl = document.querySelector(".weather-container");
+var cityListEl = document.querySelector("#city-list");
+var deleteButton = document.querySelector(".delete-button");
+var searchButton = document.querySelector(".search-button");
+
